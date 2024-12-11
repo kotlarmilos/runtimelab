613cb8b480b4d9e48809b3c7b04eaf34f08c484f
Milos Kotlar <kotlarmilos@gmail.com>
2024-12-11 18:50:14 +0100

    Fix build command

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build --projects %cd%\src\Swift.Bindings\src\Swift.Bindings.csproj %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -projects %cd%\src\Swift.Bindings\src\Swift.Bindings.csproj %*"
 exit /b %ErrorLevel%
